6118c42630c6840342de04f75af011f2b1e8253c
Anjali-herur <anjali54siri@gmail.com>
2026-07-02 10:36:35 +0530

    Create MultiplyNumbers.java

---
+++ MultiplyNumbers.java
@@ -0,0 +1,13 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        // Define two integer numbers
+        int num1 = 15;
+        int num2 = 4;
+        
+        // Multiply the numbers
+        int product = num1 * num2;
+        
+        // Display the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
