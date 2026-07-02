61cc14d237de9fda7460f7614ca18af7dacb4624
Anjali-herur <anjali54siri@gmail.com>
2026-07-02 10:37:22 +0530

    Create DivisionExample.java

---
+++ DivisionExample.java
@@ -0,0 +1,12 @@
+public class DivisionExample {
+    public static void main(String[] args) {
+        double dividend = 10.0;
+        double divisor = 4.0;
+        
+        // Performs exact division
+        double quotient = dividend / divisor; 
+        
+        // Output: 10.0 / 4.0 = 2.5
+        System.out.println(dividend + " / " + divisor + " = " + quotient);
+    }
+}
